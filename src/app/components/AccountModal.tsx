f05ad20deb2bc33c7427f49aedf01aa2bdda8645
pascal-tombere <pascaltombere1@gmal.com>
2025-03-27 18:05:04 +0100

    add copy text button

--- src/app/components/AccountModal.tsx
+++ src/app/components/AccountModal.tsx
@@ -36,7 +36,21 @@ const AccountModal: React.FC<ModalProps> = ({ isOpen, onClose, onSent }) => {
               <span>Name:</span> <span>Pascal Eno Tombere</span>
             </p>
             <p className="flex justify-between">
-              <span>Account Number:</span> <span className="font-semibold">8031903069</span>
+              <span>Account Number:</span> 
+              <span className="font-semibold flex items-center">
+                8031903069
+                <button 
+                  onClick={() => {
+                    navigator.clipboard.writeText('8031903069');
+                    alert('Account number copied to clipboard!');
+                  }}
+                  className="ml-2 text-blue-500 hover:text-blue-700"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+                  </svg>
+                </button>
+              </span>
             </p>
             <p className="flex justify-between">
               <span>Bank Name:</span> <span>Opay</span>
